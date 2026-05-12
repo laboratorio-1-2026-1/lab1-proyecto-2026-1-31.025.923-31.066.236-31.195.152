ba87d327545c0646b57477e665d3e0b29c1b1358
Yoge <1001.31066236.ucla@gmail.com>
2026-05-12 08:32:59 -0400

    octavo commit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./swagger');
 require('dotenv').config();
 
 const authRoutes = require('./routes/authRoutes');
@@ -12,6 +14,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/maquinas', maquinaRoutes);
